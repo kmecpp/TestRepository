ff2f1db91b2a46a19940af71da631aad3a23648e
kmecpp <kmecpp@gmail.com>
2014-10-25 14:13:29 -0700

    Edited header and made the actual program!

--- Main.py
+++ Main.py
@@ -1,3 +1,17 @@
 # Author: kmecpp
 # - Some Amazhing Python Code -
-# Jk this is just a test
+# (Jk this is just a test)
+
+def main():
+    try:
+        x = eval(input("Enter a number: "))
+        print(str(x) + " squared equals, " + str(x*x))
+    except (NameError, SyntaxError):
+        inputError()
+        
+def inputError():
+    print("I said enter number yah potato!!")
+    input()
+    main()
+
+main()
